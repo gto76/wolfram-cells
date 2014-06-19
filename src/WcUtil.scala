6921990b6b0d35d0c500ae19bf7f3cfc6672a308
Jure Sorn <sornjure@gmail.com>
2014-06-19 14:57:25 +0200

    Big cleanup, and command line interface implemented.

--- src/WcUtil.scala
+++ src/WcUtil.scala
@@ -1,9 +1,45 @@
-import si.gto76.funphototime.Utility
+//import si.gto76.funphototime.Utility
+
+import java.awt.image.BufferedImage
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import javax.swing.JLabel
+import javax.swing.JOptionPane
+import javax.swing.ImageIcon
 
 object WcUtil {
+	
+	def getNewMatrix(matrixHeight: Int, matrixWidth: Int): Array[Array[Boolean]] = {
+	    var matrix = Array.ofDim[Boolean](matrixHeight, matrixWidth) 
+	    matrix(0)(matrixWidth/2) = true
+	    matrix
+    }
+	
+	def getNewImage(x: Int, y: Int): BufferedImage = {
+	    val imgOut = new BufferedImage(x, y, BufferedImage.TYPE_INT_RGB)
+		imgOut;
+	}
+	
+	def printMatrix(matrix: Array[Array[Boolean]]) {
+	    print(matrixToString(matrix, "#", " "))
+	}
+		
+	def stdinToString = {
+		val sb = new StringBuilder
+		val stdin = new BufferedReader(new InputStreamReader(System.in));
+		var line: String = ""
+		while ((line = stdin.readLine()) != null && line.length() != 0) {
+			sb.append(line)
+		}
+		sb.toString
+	}
+	
+	/*
+	 * CONVERSIONS:
+	 */
     
-    def getImage(matrix: Array[Array[Boolean]]) = {
-        var img = Utility.declareNewBufferedImage(matrix(0).size, matrix.size)
+    def matrixToImage(matrix: Array[Array[Boolean]]) = {
+        var img = getNewImage(matrix(0).size, matrix.size)
         var x, y = 0;
         for (row <- matrix) {
 	        for (element <- row) {
@@ -19,11 +55,22 @@ object WcUtil {
 		img
     }
     
-	def printMatrix(matrix: Array[Array[Boolean]]) {
-	    print(matrixToString(matrix, "#", " "))
-	}
-	
-	def matrixToString(matrix: Array[Array[Boolean]], t: String, f: String) {
+    def imageToMatrix(image: BufferedImage) = {
+    	val height = image.getHeight()
+    	val width = image.getWidth()
+    	val matrix = Array.ofDim[Boolean](height, width)
+    	///
+    	for (i <- 0 to height-1) {
+	        for (j <- 0 to width-1) {
+	        	if (image.getRGB(i, j) != 0) {
+	        		matrix(i)(j) = true
+	        	}
+	        }
+	    }
+	    matrix
+    }
+
+    def matrixToString(matrix: Array[Array[Boolean]], t: String, f: String) = {
 		val sb = new StringBuilder
 		for (row <- matrix) {
 	        for (element <- row) {
@@ -36,6 +83,28 @@ object WcUtil {
 	    }
 		sb.toString
 	}
+	
+	def stringToMatrix(matrixStr: String) = {
+	    val lines = matrixStr.split("\n")
+	    val sortedLines = lines.sortWith((A, B) => A.size > B.size)
+	    val width = sortedLines(0).length
+	    val height = lines.length
+	    val matrix = Array.ofDim[Boolean](height, width)
+	    ///
+	    for (i <- 0 to height-1) {
+	        val line = lines(i)
+	        for (j <- 0 to line.length-1) {
+	        	if (line.charAt(j) != ' ') {
+	        		matrix(i)(j) = true
+	        	}
+	        }
+	    }
+	    matrix
+	}
+	
+	/*
+	 * RULES UTILS
+	 */
 	
 	def getAllCombos(len: Int): Array[String] = {
         val noOfCombos = Math.pow(2, len).asInstanceOf[Int]
@@ -60,16 +129,39 @@ object WcUtil {
 	}
 	
 	def letterToBool(letter: Char): Boolean = {
-        if (letter == '1')
-            true
-        else
-            false
+        letter == '1'
     }
+		
+	def getBinary(ruleNo: Int, length: Int): String = {
+		val binString = ruleNo.toBinaryString
+		println(binString)
+		if (binString.length() > length) {
+			binString.substring(length)
+		} else {
+			val delta = length - binString.length()
+			val sb = new StringBuilder
+			for (i <- 1 to delta) {
+				sb.append("0")
+			}
+			println(delta + " " + sb.toString + binString)
+			sb.toString + binString
+		}
+	}
 	
-	def getNewMatrix(matrixHeight: Int, matrixWidth: Int): Array[Array[Boolean]] = {
-        var matrix = Array.ofDim[Boolean](matrixHeight, matrixWidth) 
-	    matrix(0)(matrixWidth/2) = true
-	    matrix
-    }
+	//////
+		
+	def countTrues(matrix: Array[Array[Boolean]]): Int = {
+		var trues = 0
+		for (row <- matrix) {
+			for (cell <- row) {
+				if (cell) trues = trues + 1
+			}
+		}
+		trues
+	}
+	
+	def showImage(image: BufferedImage) {
+		JOptionPane.showMessageDialog(null, new JLabel(new ImageIcon(image)));
+	}
 	
 }
--- src/WolframCells.scala
+++ src/WolframCells.scala
@@ -1,101 +1,199 @@
-object WolframCells extends App {
+import java.awt.image.BufferedImage
+import org.apache.commons.cli.Options
+import org.apache.commons.cli.CommandLineParser
+import org.apache.commons.cli.CommandLine
+import org.apache.commons.cli.GnuParser
+import org.apache.commons.cli.OptionBuilder
+import java.nio.ByteBuffer
+import java.io.File
+import javax.imageio.ImageIO
+import java.io.ByteArrayOutputStream
+import java.util.Arrays
+import java.io.ByteArrayInputStream
+import java.nio.charset.Charset
+import java.io.PrintStream
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
 
-    override def main(args: Array[String]) {
-        val HEIGHT: Int = 20
-	    val WIDTH: Int = 80
-	    val TABLE_HEIGHT: Int = 2
-		val TABLE_WIDTH: Int = 3
-	    val PROCESS_SINGLE = true
-    
-	    if (PROCESS_SINGLE)
-	        processSingle(TABLE_HEIGHT, TABLE_WIDTH, HEIGHT, WIDTH)
-	    else
-	    	processAll(TABLE_HEIGHT, TABLE_WIDTH, HEIGHT, WIDTH)
-    }
-    
-    def processSingle(height: Int , width: Int, 
-            matrixHeight: Int, matrixWidth: Int ) {
-    	var matrix = WcUtil.getNewMatrix(matrixHeight, matrixWidth)
-        //var table = Array.ofDim[Boolean](height, width)
-        val table = createTable("101111", height, width)
-	    /*table(0)(0) = true
-	    table(0)(1) = true
-	    table(0)(2) = true
-	    table(1)(0) = false
-	    table(1)(1) = false
-	    table(1)(2) = false*/
-	    process(table, matrix)
-    }
-    
-    def processAll(height: Int , width: Int, 
-            matrixHeight: Int, matrixWidth: Int ) {
-        for (combo <- WcUtil.getAllCombos(height * width)) {
-        	var matrix = WcUtil.getNewMatrix(matrixHeight, matrixWidth)
-            println(combo)
-	        val table = createTable(combo, height, width)
-	        process(table, matrix)
-	    }
-    }
-    
+object WolframCells {
+	val TEXT_WIDTH = 120
+	val TEXT_HEIGHT = 50
+	val BITMAP_WIDTH = 10000
+	val BITMAP_HEIGHT = 5000
+	val INPUT = "-"
+	val OUTPUT = "-"
+	val RULE_NO = 118
+	val RULE_LENGTH = 7
 
-    
-    def process(table: Array[Array[Boolean]], matrix: Array[Array[Boolean]]) {
-        val matrixOut = runAlgorithm(table, matrix)
-	    println("+++"+table.size+"/"+table(0).size+"/"+table(1).size)
-        WcUtil.printMatrix(table)
-        println("+++")
-	    WcUtil.printMatrix(matrixOut) 
-    }
-    
-    def createTable(combo: String, y: Int, x: Int) = {
-        var table = Array.ofDim[Boolean](y, x)
-        var i = 0
-        var j = 0
-        for (letter <- combo.toCharArray) {
-            table(i)(j) = WcUtil.letterToBool(letter)
-            if (j < x-1) {
-                j = j + 1
-            }
-            else {
-            	j = 0
-            	i = i + 1
-            }
-        }
-        table
-    }
-    
-	def runAlgorithm(table: Array[Array[Boolean]], matrixIn: Array[Array[Boolean]]) = {
-	    var matrix = matrixIn.clone()
-		for (y <- 0 to matrix.size-2) {
-			for (x <- 0 to matrix(0).size-1) {
-				if (matrix(y)(x) == true) {
+	/*
+	 * Random rules:
+	 * 1111000 = 1101010 mirrored -> no 20
+	 * 0111110 = 1110110 mirrored -> halved with a path
+	 */
 
-					for (xx <- 0 to table(0).size-1) {
-						if (table(0)(xx) == true) {
-						    matrix = writeLine(y+1, x-xx, table(1), matrix)
-						}
-					}
-				
-				}
- 			}
-		}
-		matrix
+	def main(args: Array[String]) {
+		val cmd = getCommandLine(args)
+		val (bitmap, only_developed, rule_number, input, output, width, height, stdin) = setParameters(cmd)
+		val matrixIn = obtainMatrix(input, width, height, stdin)
+		val matrixOut = transformMatrix(matrixIn, rule_number)
+		val matrixIsNotDeveloped = !isDeveloped(matrixOut)
+		if (only_developed && matrixIsNotDeveloped) return
+		dispatchMatrix(matrixOut, bitmap, output)
 	}
 	
-	def writeLine(y: Int, x: Int, tableRow: Array[Boolean],  matrixIn: Array[Array[Boolean]]) = {
-		var matrix = matrixIn.clone()
-	    for (xx <- 0 to tableRow.size-1) { 
-			val xxx=x+xx
-		    if ( (xxx >= 0) && (xxx < matrix(0).size) && (tableRow(xx)==true) ) {
-		    	//matrix(y)(xxx) = tableRow(xx)
-		        if (matrix(y)(xxx))
-		            matrix(y)(xxx) = false
-		            else
-		        matrix(y)(xxx) = true
-		    }
+	////////////////////////////////////
+	
+	
+	/*
+	 * GET COMMAND LINE
+	 */
+	
+	private def getCommandLine(args: Array[String]): CommandLine = {
+		val options = getOptions
+		val parser: CommandLineParser = new GnuParser();
+		val cmd: CommandLine = parser.parse(options, args);
+		cmd
+	}
+	
+	def getOptions: Options = {
+		val options = new Options()
+		options.addOption("b", "bitmap", false, "Outupt image instead of text.")
+		options.addOption("o", "only-developed", false, "Output matrix only if there is more than one cell per row.")
+		addOptionWithArg(options, 'r', "rule-number", "Rule to be used.")
+		addOptionWithArg(options, 'w', "width", "Matrix width")
+		addOptionWithArg(options, 'h', "height", "Matrix height.")
+		addOptionWithArg(options, 'i', "input-matrix", "Specify filename containing matrix on which the " +
+			" transformations will be exexuted. Should be in text format or in png.")
+		addOptionWithArg(options, 'f', "file", "Output filename.")
+		options
+	}
+
+	def addOptionWithArg(options: Options, shortName: Char, longName: String, description: String) {
+		OptionBuilder.withLongOpt(longName)
+		OptionBuilder.hasArg
+		OptionBuilder.withDescription(description)
+		val option = OptionBuilder.create('r')
+		options.addOption(option);
+	}
+	
+	/*
+	 * SET PARAMETERS
+	 */
+	
+	private def setParameters(cmd: org.apache.commons.cli.CommandLine): (Boolean, Boolean, Int, String, String, Int, Int, String) = {
+		val bitmap = cmd.hasOption("b")
+		val only_developed = cmd.hasOption("o")
+		val rule_number = if (cmd.hasOption("r")) cmd.getOptionValue("r").toInt else RULE_NO
+		val input = if (cmd.hasOption("i")) cmd.getOptionValue("i") else INPUT
+		val output = if (cmd.hasOption("o")) cmd.getOptionValue("o") else OUTPUT
+		val width = if (cmd.hasOption("w")) cmd.getOptionValue("w").toInt else if (bitmap) BITMAP_WIDTH else TEXT_WIDTH
+		val height = if (cmd.hasOption("h")) cmd.getOptionValue("h").toInt else if (bitmap) BITMAP_HEIGHT else TEXT_HEIGHT
+		val stdin = WcUtil.stdinToString
+		(bitmap, only_developed, rule_number, input, output, width, height, stdin)
+	}
+
+	/*
+	 * OBTAIN MATRIX
+	 */
+
+	private def obtainMatrix(input: String, width: Int, height: Int, stdin: String): Array[Array[Boolean]] = {
+		val thereIsdataOnStdin = stdin.length() != 0
+		val inputFilenameWasSpecified = input != "-"
+		if (thereIsdataOnStdin) {
+			WcUtil.stringToMatrix(stdin)
+		} else if (inputFilenameWasSpecified) {
+			getMatrixFromFile(input)
+		} else {
+			WcUtil.getNewMatrix(height, width)
 		}
+	}
+
+	private def getMatrixFromFile(input: String): Array[Array[Boolean]] = {
+		val fileIsImage = input.toLowerCase.endsWith(".png")
+		if (fileIsImage) {
+			val image = ImageIO.read(new File(input))
+			WcUtil.imageToMatrix(image)
+		} else {
+			val source = scala.io.Source.fromFile(input)
+			val matrixStr = source.mkString
+			source.close()
+			WcUtil.stringToMatrix(matrixStr)
+		}
+	}
+	
+	/*
+	 * TRANSFORM MATRIX
+	 */
+
+	def transformMatrix(matrix: Array[Array[Boolean]], ruleNo: Int): Array[Array[Boolean]] = {
+		val ruleBinary = WcUtil.getBinary(ruleNo, RULE_LENGTH)
+		val ruleArray = WcUtil.stringToBoolArray("0" + ruleBinary)
+		val rule = new Rule(ruleArray)
+		println(ruleNo + " " + ruleBinary + " " + ruleArray + " " + rule)
+		processMatrix(matrix, rule)
 		matrix
 	}
 
+	class Rule(rules: Array[Boolean]) {
+		def getCell(neighbours: Array[Boolean]): Boolean = {
+			val index = toInt(neighbours(0), neighbours(1), neighbours(2))
+			rules(index)
+		}
+		def toInt(b1: Boolean, b2: Boolean, b3: Boolean): Int = toInt(b1) * 4 + toInt(b2) * 2 + toInt(b3)
+		def toInt(b: Boolean): Int = if (b) 1 else 0
+	}
 	
+	def processMatrix(matrix: Array[Array[Boolean]], rule: Rule) {
+		for (y <- 0 to matrix.size - 2) {
+			for (x <- 0 to matrix(0).size - 3) {
+				val slice = matrix(y).slice(x, x + 3)
+				val newCell = rule.getCell(slice)
+				matrix(y + 1)(x + 1) = newCell
+			}
+		}
+	}
+
+	/*
+	 * IS DEVELOPED
+	 */
+	
+	def isDeveloped(matrix: Array[Array[Boolean]]): Boolean = {
+		val noOfTrues = WcUtil.countTrues(matrix)
+		val matrixHeight = matrix.size
+		return noOfTrues > matrixHeight * 2
+	}
+	
+	/*
+	 * DISPATCH MATRIX
+	 */
+
+	def dispatchMatrix(matrix: Array[Array[Boolean]], bitmap: Boolean, output: String) {
+		if (bitmap) {
+			dispatchBitmap(matrix, output)
+		} else {
+			dispatchText(matrix, output)
+		}
+	}
+
+	private def dispatchBitmap(matrix: Array[Array[Boolean]], output: String): Unit = {
+		val img = WcUtil.matrixToImage(matrix)
+		if (output == "-") {
+			ImageIO.write(img, "png", System.out);
+		} else {
+			ImageIO.write(img, "png", new File(output));
+		}
+	}
+	
+	private def dispatchText(matrix: Array[Array[Boolean]], output: String): Unit = {
+		if (output == "-") {
+			WcUtil.printMatrix(matrix)
+		} else {
+			val matrixStr = WcUtil.matrixToString(matrix, "#", " ")
+			val out = new PrintWriter(output)
+			out.println(matrixStr)
+		}
+	}
+
+
 }
